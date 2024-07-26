f5c392de0db8d7dca94bfd7d75e4b432eb84a32b
Francois Maillet <fmaillet@gmail.com>
2024-07-26 13:40:01 -0400

    recursion

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 use geo::{
     algorithm::{centroid::Centroid, contains::Contains},
     BoundingRect, Coord, Intersects, LineString, Polygon,
