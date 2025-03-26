42e786761a5475cf4836891a586c07ed22b5aa65
Jean Raby <jean@raby.sh>
2025-03-26 12:44:51 -0400

    move algorithm logic to function

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,75 @@ use geo::{
     BoundingRect, Intersects, Polygon,
 };
 use geo_types::Geometry as GtGeometry;
-use geohash::{decode_bbox, encode, neighbors};
+use geohash::{decode_bbox, encode, neighbors, GeohashError};
 use py_geo_interface::Geometry;
-use pyo3::prelude::*;
 use pyo3::types::PyAny;
 use pyo3::wrap_pyfunction;
+use pyo3::{exceptions::PyValueError, prelude::*};
 use std::collections::{HashSet, VecDeque};
+
+fn polygons_to_geohashes(
+    polygons: Vec<Polygon>,
+    precision: usize,
+    inner: bool,
+) -> Result<HashSet<String>, GeohashError> {
+    let mut inner_geohashes = HashSet::new();
+    let mut outer_geohashes = HashSet::new();
+
+    for polygon in &polygons {
+        let envelope = polygon.bounding_rect().unwrap();
+
+        let centroid = polygon.centroid().unwrap();
+        let centroid_geohash = encode((centroid.x(), centroid.y()).into(), precision)?;
+
+        let mut testing_geohashes = VecDeque::new();
+        testing_geohashes.push_back(centroid_geohash);
+
+        while let Some(current_geohash) = testing_geohashes.pop_front() {
+            if inner_geohashes.contains(&current_geohash)
+                || outer_geohashes.contains(&current_geohash)
+            {
+                continue;
+            }
+
+            let rect_bbox = decode_bbox(&current_geohash)?;
+            let current_geohash_polygon = rect_bbox.to_polygon();
+
+            let condition = if inner {
+                envelope.contains(&rect_bbox)
+            } else {
+                envelope.intersects(&rect_bbox)
+            };
+            if !condition {
+                continue;
+            }
+
+            if inner {
+                if polygon.contains(&current_geohash_polygon) {
+                    inner_geohashes.insert(current_geohash.clone());
+                } else {
+                    outer_geohashes.insert(current_geohash.clone());
+                }
+            } else {
+                if polygon.intersects(&current_geohash_polygon) {
+                    inner_geohashes.insert(current_geohash.clone());
+                } else {
+                    outer_geohashes.insert(current_geohash.clone());
+                }
+            }
+
+            if let Ok(rez) = neighbors(&current_geohash) {
+                for neighbor in [rez.sw, rez.s, rez.se, rez.w, rez.e, rez.nw, rez.n, rez.ne] {
+                    if !inner_geohashes.contains(&neighbor) && !outer_geohashes.contains(&neighbor)
+                    {
+                        testing_geohashes.push_back(neighbor.to_string());
+                    }
+                }
+            }
+        }
+    }
+    Ok(inner_geohashes)
+}
 
 #[pyfunction]
 fn polygon_to_geohashes(
@@ -17,9 +80,6 @@ fn polygon_to_geohashes(
     precision: usize,
     inner: bool,
 ) -> PyResult<HashSet<String>> {
-    let mut inner_geohashes = HashSet::new();
-    let mut outer_geohashes = HashSet::new();
-
     let mut polygons = Vec::<Polygon<f64>>::new();
 
     let geom: Geometry = match py_polygon.extract::<Geometry>() {
@@ -49,60 +109,8 @@ fn polygon_to_geohashes(
         return Err(pyo3::exceptions::PyValueError::new_err(e));
     }
 
-    for polygon in &polygons {
-        let envelope = polygon.bounding_rect().unwrap();
-        let poly_envelope = envelope.to_polygon();
-
-        let centroid = polygon.centroid().unwrap();
-        let centroid_geohash = encode((centroid.x(), centroid.y()).into(), precision)
-            .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("{:?}", e)))?;
-
-        let mut testing_geohashes = VecDeque::new();
-        testing_geohashes.push_back(centroid_geohash);
-
-        while let Some(current_geohash) = testing_geohashes.pop_front() {
-            if !inner_geohashes.contains(&current_geohash)
-                && !outer_geohashes.contains(&current_geohash)
-            {
-                let rect_bbox = decode_bbox(&current_geohash)
-                    .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("{:?}", e)))?;
-                let current_polygon = rect_bbox.to_polygon();
-
-                let condition = if inner {
-                    poly_envelope.contains(&current_polygon)
-                } else {
-                    poly_envelope.intersects(&current_polygon)
-                };
-                if condition {
-                    if inner {
-                        if polygon.contains(&current_polygon) {
-                            inner_geohashes.insert(current_geohash.clone());
-                        } else {
-                            outer_geohashes.insert(current_geohash.clone());
-                        }
-                    } else {
-                        if polygon.intersects(&current_polygon) {
-                            inner_geohashes.insert(current_geohash.clone());
-                        } else {
-                            outer_geohashes.insert(current_geohash.clone());
-                        }
-                    }
-
-                    if let Ok(rez) = neighbors(&current_geohash) {
-                        for neighbor in [rez.sw, rez.s, rez.se, rez.w, rez.e, rez.nw, rez.n, rez.ne]
-                        {
-                            if !inner_geohashes.contains(&neighbor)
-                                && !outer_geohashes.contains(&neighbor)
-                            {
-                                testing_geohashes.push_back(neighbor.to_string());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Ok(inner_geohashes)
+    polygons_to_geohashes(polygons, precision, inner)
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("{:?}", e)))
 }
 
 #[pymodule]
