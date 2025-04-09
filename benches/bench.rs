bf99e2a9a8cc14f0a62721eef54f95971ba85477
Jean Raby <jean@raby.sh>
2025-04-09 11:44:41 -0400

    reorder benchmarks for easier comparison
    old, new

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,27 +9,27 @@ fn bench_polygons_to_geohashes(c: &mut Criterion) {
 
     let wh = include_str!("../tests/data/whitehorse_wkt.txt");
     let wh: MultiPolygon<f64> = MultiPolygon::try_from_wkt_str(wh).unwrap();
-    c.bench_function("verdun 7 false", |b| {
-        b.iter(|| polygons_to_geohashes(verdun.clone(), 7, false))
-    });
-    c.bench_function("verdun 7 true", |b| {
-        b.iter(|| polygons_to_geohashes(verdun.clone(), 7, true))
-    });
     c.bench_function("verdun 7 false oldfunc", |b| {
         b.iter(|| polygons_to_geohashes_handbrake(verdun.clone(), 7, false))
     });
+    c.bench_function("verdun 7 false", |b| {
+        b.iter(|| polygons_to_geohashes(verdun.clone(), 7, false))
+    });
     c.bench_function("verdun 7 true oldfunc", |b| {
         b.iter(|| polygons_to_geohashes_handbrake(verdun.clone(), 7, true))
+    });
+    c.bench_function("verdun 7 true", |b| {
+        b.iter(|| polygons_to_geohashes(verdun.clone(), 7, true))
     });
 
     c.bench_function("whitehorse 6 false oldfunc", |b| {
         b.iter(|| polygons_to_geohashes_handbrake(wh.clone(), 6, false))
     });
-    c.bench_function("whitehorse 6 true oldfunc", |b| {
-        b.iter(|| polygons_to_geohashes_handbrake(wh.clone(), 6, true))
-    });
     c.bench_function("whitehorse 6 false", |b| {
         b.iter(|| polygons_to_geohashes(wh.clone(), 6, false))
+    });
+    c.bench_function("whitehorse 6 true oldfunc", |b| {
+        b.iter(|| polygons_to_geohashes_handbrake(wh.clone(), 6, true))
     });
     c.bench_function("whitehorse 6 true", |b| {
         b.iter(|| polygons_to_geohashes(wh.clone(), 6, true))
